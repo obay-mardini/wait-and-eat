8d4b2de6aab2a4237088c3f0d8429c837a7a9720
obay2 <obaymardini@gmail.com>
2016-08-13 23:46:42 +0200

    make a form and add an eventListener to the submit button

---
+++ src/app/waitList/waitList.controller.js
@@ -0,0 +1,18 @@
+(function() {
+  'use strict';
+
+
+    angular.module('app.waitList').controller('WaitListController',WaitListController);
+
+  function WaitListController() {
+      var vm = this;
+      vm.parties = [1,2,3];
+      
+      function addParty() {
+          vm.parties.push('another');
+      }
+      
+      vm.addParty = addParty;
+  }
+   
+})();
