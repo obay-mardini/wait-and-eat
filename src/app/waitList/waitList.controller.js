1264ba1a3831155a9a46d127ac6009c3309257d6
obay2 <obaymardini@gmail.com>
2016-08-14 11:35:59 +0200

    make a table showing the data using ng-repeat

--- src/app/waitList/waitList.controller.js
+++ src/app/waitList/waitList.controller.js
@@ -1,18 +1,32 @@
 (function() {
   'use strict';
 
+ angular.module('app.waitList').controller('WaitListController',WaitListController);
+    
+    WaitListController.$inject = ['$firebaseArray']
 
-    angular.module('app.waitList').controller('WaitListController',WaitListController);
-
-  function WaitListController() {
-      var vm = this;
-      vm.parties = [1,2,3];
+  function WaitListController($firebaseArray) {
       
+      function Party() {
+          this.name = '';
+          this.phone = '';
+          this.size = '';
+          this.done = false;
+          this.notified = false;
+      }
+      var vm = this;
+     
+      var fireParties = firebase.database().ref('parties');
+      
+      vm.parties =  $firebaseArray(fireParties);
+      vm.newParty = new Party();
       function addParty() {
-          vm.parties.push('another');
+          vm.parties.$add(vm.newParty);
+          vm.newParty = new Party();
       }
       
       vm.addParty = addParty;
+      
   }
    
 })();
