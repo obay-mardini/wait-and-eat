ce5bd0920d857e536236669490a3ccb678f2314c
obay2 <obaymardini@gmail.com>
2016-09-17 12:33:08 +0200

    catch unknown URLs

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -1,16 +1,27 @@
 (function() {
     'use strict'
     
-   angular.module("app", [
-    'ngRoute',
-    'firebase',
-    //custom modules.
-    'app.waitList',
-    'app.landing',
-    'app.auth',
-    'app.core',
-    'app.layout'
-   ]); 
+   angular
+       .module("app", [
+        'ngRoute',
+        'firebase',
+        //custom modules.
+        'app.waitList',
+        'app.landing',
+        'app.auth',
+        'app.core',
+        'app.layout'
+       ])
+        .config(configurationFunction);
+   
     
+    
+    configurationFunction.$inject = ['$routeProvider'];
+    
+    function configurationFunction($routeProvider) {
+        $routeProvider.otherwise({
+            redirectTo: '/'
+        });
+    }
 })();
     
